45bbb81a0e47c2852810d96918a49fde8a53f34e
Jonn Alves <jonatas.alsantos@jsl.com.br>
2022-02-07 15:32:41 -0300

    Initial commit

---
+++ app/src/main/java/br/com/appfactory/webviewandclipboard/MainActivity.kt
@@ -0,0 +1,41 @@
+package br.com.appfactory.webviewandclipboard
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.addJavascriptInterface(WebAppInterface(this), "Android")
+        myWebView.loadUrl("https://www.w3schools.com/howto/howto_js_copy_clipboard.asp")
+        myWebView.addJavascriptInterface(WebAppInterface(this), "NativeAndroid")
+
+    }
+}
+
+class WebAppInterface(private val context: Context) {
+    @JavascriptInterface
+    fun copyToClipboard(text: String?) {
+        val clipboard: ClipboardManager? =
+            context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+        val clip = ClipData.newPlainText("demo", text)
+        clipboard?.setPrimaryClip(clip)
+    }
+
+    /** Show a toast from the web page  */
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(context, toast, Toast.LENGTH_SHORT).show()
+    }
+}
